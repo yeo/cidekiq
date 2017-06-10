77eafdd693e8dd3ab73976de0c8487b1c04a0d23
Vinh Nguyen <kurei@axcoto.com>
2017-06-10 12:07:24 -0700

    update project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cidekiq "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A SideKiq client to queue/scedule job to sidekiq direcly"
+  :url "https://noty.im"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
